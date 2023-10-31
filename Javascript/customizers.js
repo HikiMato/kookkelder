b2ec521fa910ff5eac096887ef419eb2996f0b99
HikiMato <danielsandzand@gmail.com>
2023-10-31 10:24:36 +0100

    notification done

--- Javascript/customizers.js
+++ Javascript/customizers.js
@@ -1,67 +1,70 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const backgroundColorPickers = document.querySelectorAll('.bgcolor');
-    const backgroundColorPicker = document.getElementById('backgroundColorPicker');
-    
-    const textColorPickers = document.querySelectorAll('.txtcolor');
-    const textColorPicker = document.getElementById('textColorPicker');
+document.addEventListener("DOMContentLoaded", function () {
+  const backgroundColorPickers = document.querySelectorAll(".bgcolor");
+  const backgroundColorPicker = document.getElementById(
+    "backgroundColorPicker"
+  );
 
-    // Listen for background color changes
-    backgroundColorPicker.addEventListener('input', handleBackgroundColorChange);
+  const textColorPickers = document.querySelectorAll(".txtcolor");
+  const textColorPicker = document.getElementById("textColorPicker");
 
-    // Listen for text color changes
-    textColorPicker.addEventListener('input', handleTextColorChange);
+  // Listen for background color changes
+  backgroundColorPicker.addEventListener("input", handleBackgroundColorChange);
 
-    function handleBackgroundColorChange(event) {
-        const backgroundColor = event.target.value;
-        backgroundColorPickers.forEach(element => {
-            element.style.backgroundColor = backgroundColor;
-        });
+  // Listen for text color changes
+  textColorPicker.addEventListener("input", handleTextColorChange);
 
-        // Save the selected background color to local storage
-        localStorage.setItem('selectedBackgroundColor', backgroundColor);
-    }
+  function handleBackgroundColorChange(event) {
+    const backgroundColor = event.target.value;
+    backgroundColorPickers.forEach((element) => {
+      element.style.backgroundColor = backgroundColor;
+    });
 
-    function handleTextColorChange(event) {
-        const textColor = event.target.value;
-        textColorPickers.forEach(element => {
-            element.style.color = textColor;
-        });
+    // Save the selected background color to local storage
+    localStorage.setItem("selectedBackgroundColor", backgroundColor);
+  }
 
-        // Save the selected text color to local storage
-        localStorage.setItem('selectedTextColor', textColor);
-    }
+  function handleTextColorChange(event) {
+    const textColor = event.target.value;
+    textColorPickers.forEach((element) => {
+      element.style.color = textColor;
+    });
 
-    const savedBackgroundColor = localStorage.getItem('selectedBackgroundColor') || '#008489';
-    const savedTextColor = localStorage.getItem('selectedTextColor') || 'white';
+    // Save the selected text color to local storage
+    localStorage.setItem("selectedTextColor", textColor);
+  }
 
-    // Apply the saved colors
-    backgroundColorPicker.value = savedBackgroundColor;
-    textColorPicker.value = savedTextColor;
-    handleBackgroundColorChange({ target: { value: savedBackgroundColor } }); // Apply initial background color
-    handleTextColorChange({ target: { value: savedTextColor } }); // Apply initial text color
+  const savedBackgroundColor =
+    localStorage.getItem("selectedBackgroundColor") || "#008489";
+  const savedTextColor = localStorage.getItem("selectedTextColor") || "white";
+
+  // Apply the saved colors
+  backgroundColorPicker.value = savedBackgroundColor;
+  textColorPicker.value = savedTextColor;
+  handleBackgroundColorChange({ target: { value: savedBackgroundColor } }); // Apply initial background color
+  handleTextColorChange({ target: { value: savedTextColor } }); // Apply initial text color
 });
 
-window.onload = function() {
-    var savedLogo = localStorage.getItem('uploadedLogo');
-    if (savedLogo) {
-        document.getElementById('uploaded-logo').src = savedLogo;
-    }
+window.onload = function () {
+  var savedLogo = localStorage.getItem("uploadedLogo");
+  if (savedLogo) {
+    document.getElementById("uploaded-logo").src = savedLogo;
+  }
 };
 
 function displayLogo() {
-    var logo = document.getElementById('uploaded-logo');
-    var input = document.getElementById('logo-upload');
-    var file = input.files[0];
+  var logo = document.getElementById("uploaded-logo");
+  var input = document.getElementById("logo-upload");
+  var file = input.files[0];
 
-    if (file) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-            // Set logo source to the data URL
-            logo.src = e.target.result;
+  if (file) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      // Set logo source to the data URL
+      logo.src = e.target.result;
 
-            // Save data URL to local storage
-            localStorage.setItem('uploadedLogo', e.target.result);
-        }
-        reader.readAsDataURL(file);
-    }
+      // Save data URL to local storage
+      localStorage.setItem("uploadedLogo", e.target.result);
+    };
+    reader.readAsDataURL(file);
+  }
 }
